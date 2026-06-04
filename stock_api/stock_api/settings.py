958840c3d44bd7818cfc92a2d9a2dac59bddbd40
jrtewani <juniortewani75@gmail.com>
2026-06-04 19:39:24 +0100

    migration sur postgresql

--- stock_api/stock_api/settings.py
+++ stock_api/stock_api/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'stock_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestion_api',
+        'USER': 'postgres',
+        'PASSWORD': 'tewani200',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
